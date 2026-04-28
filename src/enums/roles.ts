206787f58adb45b48655a5ee6fa8a52fe30f0364
Глеб Нюнько <nunkogleb348@gmail.com>
2026-04-28 22:50:49 +0300

    User type was changed, roles enum was added

---
+++ src/enums/roles.ts
@@ -0,0 +1,6 @@
+export enum ROLES {
+  CO_FOUNDER = 'co-founder',
+  LOGISTICIAN = 'logistician',
+  EXPEDITOR = 'expeditor',
+  COURIER = 'сourier',
+}
--- src/types/User.ts
+++ src/types/User.ts
@@ -1,9 +1,13 @@
+import { ROLES } from 'src/enums/roles';
+
 export interface User {
   id: string;
   email: string;
   username: string;
   name: string;
   surname: string;
+  role: ROLES | null;
+  isAdmin: boolean;
   company_id: string | null;
 }
 export interface UserWithPassword extends User {
