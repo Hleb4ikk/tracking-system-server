6b98f4f798d9eb374436d49cc2ee7e30cc46cba6
Глеб Нюнько <nunkogleb348@gmail.com>
2026-05-08 18:43:06 +0300

    added trigger to save status in history table

--- migrations/sqls/20260508151758-create-status-history-table-up.sql
+++ migrations/sqls/20260508151758-create-status-history-table-up.sql
@@ -9,3 +9,27 @@ CREATE TABLE IF NOT EXISTS status_history(
     CONSTRAINT fk_order FOREIGN KEY (order_id) REFERENCES orders(id) ON DELETE CASCADE
 
 );
+
+CREATE OR REPLACE FUNCTION log_order_status_change()
+RETURNS TRIGGER AS $$
+BEGIN
+    IF TG_OP = 'INSERT' THEN
+        INSERT INTO status_history (title, company_id, order_id)
+        VALUES (NEW.status, NEW.company_id, NEW.id);
+        RETURN NEW;
+    END IF;
+    
+    IF TG_OP = 'UPDATE' AND OLD.status IS DISTINCT FROM NEW.status THEN
+        INSERT INTO status_history (title, company_id, order_id)
+        VALUES (NEW.status, NEW.company_id, NEW.id);
+        RETURN NEW;
+    END IF;
+    
+    RETURN NEW;
+END;
+$$ LANGUAGE plpgsql;
+
+CREATE TRIGGER order_status_trigger
+AFTER INSERT OR UPDATE ON orders
+FOR EACH ROW
+EXECUTE FUNCTION log_order_status_change();
